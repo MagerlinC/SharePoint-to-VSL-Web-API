d1d550c65aef12c2ed122dd04df3b280baf1aef9
Mikkel Agerlin Christensen <miach@ramboll.dk>
2018-04-05 14:39:16 +0200

    Fixed error

--- Models/GuideDTO.cs
+++ Models/GuideDTO.cs
@@ -8,14 +8,6 @@ namespace RambollProject.Models
 {
     public class GuideDTO
     {
-        public GuideDTO(int id, string title, string description, string imgurl) {
-            GuideDTOId = id;
-            this.title = title;
-            this.description = description;
-            this.imgurl = imgurl;
-            this.guideItems = null;
-        }
-
         [Key]
         public int GuideDTOId { get; set; }
         public string title { get; set; }
